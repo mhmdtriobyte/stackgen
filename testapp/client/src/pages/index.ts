b8c97433810a0c36c3601125368c0cae319d10d4
Mhmd <68063423+mhmdtriobyte@users.noreply.github.com>
2026-03-11 15:37:08 +0200

    Remove test output from repo

--- testapp/client/src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-export { HomePage } from './HomePage';
-export { DashboardPage } from './DashboardPage';
-export { SignInPage } from './SignInPage';
-export { SignUpPage } from './SignUpPage';
-export { NotFoundPage } from './NotFoundPage';
